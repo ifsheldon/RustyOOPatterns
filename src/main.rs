53acc1ddaea86faf1dad30531f358c7658d4bd56
Maple <maplevantablack@outlook.com>
2019-11-09 00:26:10 +0800

    main

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+//!
+//! The root of the project
+//!
+//! use `cargo doc --open` in cmd to see the documentation
+//!
+//! TODO: implement Singleton Pattern somehow
+//!
+use PatternsImpl::*;
+use PatternsImpl::decorator_pattern::decorators::{Milk, Condiment};
+use PatternsImpl::decorator_pattern::bases::Beverage;
+
+fn main()
+{
+    println!("Compile test")
+}
+
